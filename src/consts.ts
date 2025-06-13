6dd6438ed99abc410e231d452e509b75f846a2ba
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2025-06-13 22:53:35 +0400

    add: initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,24 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PR Wave';
+export const SITE_DESCRIPTION = 'Professional Social Media Management, Advertising, and Digital Marketing Solutions';
+export const COMPANY_NAME = 'PR Wave';
+export const COMPANY_TAGLINE = 'Riding the Wave of Digital Success';
+
+// Company Services
+export const SERVICES = [
+  'Social Media Management',
+  'Digital Advertising',
+  'Business Mentorship',
+  'Content Creation',
+  'Brand Strategy',
+  'Analytics & Reporting'
+];
+
+// Contact Information
+export const CONTACT_INFO = {
+  email: 'info@prwave.com',
+  phone: '+1 (555) 123-4567',
+  address: '123 Digital Street, Marketing City, MC 12345'
+};
